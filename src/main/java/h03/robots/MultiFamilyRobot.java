72f09ecd50f2fb9d9a23df67ed51e7e3101691ec
SobuHasy <sobu.hasy@hotmail.com>
2023-12-04 10:26:15 +0100

    H1.2 already made it

--- src/main/java/h03/robots/MultiFamilyRobot.java
+++ src/main/java/h03/robots/MultiFamilyRobot.java
@@ -1,4 +1,16 @@
 package h03.robots;
 
-public class MultiFamilyRobot {
+import fopbot.Robot;
+import fopbot.RobotFamily;
+
+public class MultiFamilyRobot extends Robot{
+    private final RobotFamily[] families;
+
+    private int current = 0;
+
+    public MultiFamilyRobot(final int x, final int y, final RobotFamily[] families){
+        super(x, y, families[0]);
+        this.families=families;
+    }
+    
 }
