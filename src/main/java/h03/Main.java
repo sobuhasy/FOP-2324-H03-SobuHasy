b80a8200911033816ee24a8b98c0183fbd3279f2
SobuHasy <sobu.hasy@hotmail.com>
2023-12-05 06:22:21 +0100

    Implemented the RGBRobots H2.1

--- src/main/java/h03/Main.java
+++ src/main/java/h03/Main.java
@@ -1,5 +1,6 @@
 package h03;
 
+import fopbot.RobotFamily;
 import fopbot.World;
 import h03.robots.MultiFamilyRobot;
 import h03.robots.RGBRobots;
@@ -14,6 +15,17 @@ public class Main {
      * @param args program arguments, currently ignored
      */
     public static void main(String[] args) {
+        final var robots = new Robot[]{
+            new MultiFamilyRobot(0, 0,
+                new RobotFamily[]{
+                    RobotFamily.SQUARE_AQUA, RobotFamily.SQUARE_BLACK, RobotFamily.SQUARE_GREEN
+                }
+            ),
+            new RGBRobot(1, 1, false),
+            new RGBRobot(1, 2, true),
+        };
+
+
         World.setVisible(true);
     }
 }
--- src/main/java/h03/robots/RGBRobots.java
+++ src/main/java/h03/robots/RGBRobots.java
@@ -5,5 +5,22 @@ import fopbot.RobotFamily;
 import static fopbot.RobotFamily.SQUARE_BLUE;
 import static fopbot.RobotFamily.SQUARE_GREEN;
 import static fopbot.RobotFamily.SQUARE_RED;
-public class RGBRobots {
+public class RGBRobots extends MultiFamilyRobot{
+    private static final RobotFamily[] RGB = new RobotFamily[][SQUARE_RED, SQUARE_GREEN, SQUARE_BLUE];
+
+    private static final RobotFamily[] BRG = new RobotFamily[][SQUARE_BLUE, SQUARE_RED, SQUARE_GREEN];
+
+    private static final RobotFamily[] BGR = new RobotFamily[][SQUARE_BLUE, SQUARE_GREEN, SQUARE_RED];
+
+    private static final RobotFamily[] GRB = new RobotFamily[][SQUARE_GREEN, SQUARE_RED, SQUARE_BLUE];
+
+    public RGBrobot(final int x, final int y, final boolean inverted){
+        super(x, y, !inverted ? RGB : BRG);
+    }
+
+    public void testRGB(){
+        for(int i = 0; i < 3; i++){
+            exchange();
+        }
+    }
 }
