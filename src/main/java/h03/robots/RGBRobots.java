99f6f3c519f8e1c3a38b999339834407077b0450
SobuHasy <sobu.hasy@hotmail.com>
2023-12-05 06:47:00 +0100

    Tried to connect these 2 classes but it failed

--- src/main/java/h03/robots/RGBRobots.java
+++ src/main/java/h03/robots/RGBRobots.java
@@ -2,20 +2,17 @@ package h03.robots;
 
 import fopbot.RobotFamily;
 
-import static fopbot.RobotFamily.SQUARE_BLUE;
-import static fopbot.RobotFamily.SQUARE_GREEN;
-import static fopbot.RobotFamily.SQUARE_RED;
-public class RGBRobots extends MultiFamilyRobot{
+import static fopBot.RobotFamily.SQUARE_BLUE;
+import static fopBot.RobotFamily.SQUARE_RED;
+import static fopBot.RobotFamily.SQUARE_GREEN;
+
+public class RGBRobot extends MultiFamilyRobot{
     private static final RobotFamily[] RGB = new RobotFamily[][SQUARE_RED, SQUARE_GREEN, SQUARE_BLUE];
 
     private static final RobotFamily[] BRG = new RobotFamily[][SQUARE_BLUE, SQUARE_RED, SQUARE_GREEN];
 
-    private static final RobotFamily[] BGR = new RobotFamily[][SQUARE_BLUE, SQUARE_GREEN, SQUARE_RED];
-
-    private static final RobotFamily[] GRB = new RobotFamily[][SQUARE_GREEN, SQUARE_RED, SQUARE_BLUE];
-
-    public RGBrobot(final int x, final int y, final boolean inverted){
-        super(x, y, !inverted ? RGB : BRG);
+    public void RGBRobot(final int x, final int y, final boolean inverted){
+        super(x, y, !inverted ? BRG : RGB);
     }
 
     public void testRGB(){
