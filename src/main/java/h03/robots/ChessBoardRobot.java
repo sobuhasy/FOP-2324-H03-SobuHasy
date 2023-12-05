419db0f89b870d0c0cd5a13b47a781602a076441
SobuHasy <sobu.hasy@hotmail.com>
2023-12-05 10:03:23 +0100

    Learned what super means in Java

--- src/main/java/h03/robots/ChessBoardRobot.java
+++ src/main/java/h03/robots/ChessBoardRobot.java
@@ -1,4 +1,9 @@
 package h03.robots;
 
-public class ChessBoardRobot {
+import fopbot.RobotFamily;
+
+public class ChessBoardRobot extends MultiFamilyRobot{
+    public ChessBoardRobot(final int x, final int y, final RobotFamily even, final RobotFamily odd){
+        super(x, y, (x + y) % 2 == 0 ? new RobotFamily[]{even, odd} : new RobotFamily[]{odd, even});
+    }
 }
