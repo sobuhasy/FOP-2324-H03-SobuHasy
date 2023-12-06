aba1227e09f73cd3c82e3a0d1fc39adaa32a8e99
SobuHasy <sobu.hasy@hotmail.com>
2023-12-06 05:22:19 +0100

    Set the position to sync the robots with

--- src/main/java/h03/RobotSynchroniser.java
+++ src/main/java/h03/RobotSynchroniser.java
@@ -4,11 +4,27 @@ import fopbot.Direction;
 import fopbot.Robot;
 import fopbot.World;
 
-import static fopbot.Direction.DOWN;
-import static fopbot.Direction.UP;
-import static fopbot.Direction.LEFT;
-import static fopbot.Direction.RIGHT;
 public class RobotSynchroniser {
     private final Robot[] robots;
+
+    private int x = -1;
+    private int y = -1;
+    private Direction direction = null;
+
+    public RobotSynchroniser(final Robot[] robots){
+        this.robots = robots;
+    }
+
+    public void setX(final int x){
+        if (x >= 0 && x < World.getWidth()){
+            this.x = x;
+        }
+    }
+
+    public void setY(final int y){
+        if(y >= 0 && y < World.getHeight()){
+            this.y = y;
+        }
+    }
 
 }
