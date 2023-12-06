e96c54d6879c858e703cf79748d2040a2e2851e8
SobuHasy <sobu.hasy@hotmail.com>
2023-12-06 05:16:06 +0100

    Implemented the class RobotSyncroniser as a public constructor

--- src/main/java/h03/RobotSynchroniser.java
+++ src/main/java/h03/RobotSynchroniser.java
@@ -1,5 +1,14 @@
 package h03;
 
+import fopbot.Direction;
+import fopbot.Robot;
+import fopbot.World;
+
+import static fopbot.Direction.DOWN;
+import static fopbot.Direction.UP;
+import static fopbot.Direction.LEFT;
+import static fopbot.Direction.RIGHT;
 public class RobotSynchroniser {
+    private final Robot[] robots;
 
 }
