94bcb2cf02113503ecc4066e677cdafffa2e858f
SobuHasy <sobu.hasy@hotmail.com>
2023-12-07 15:26:32 +0100

    Refactored the RGBRobots file into RGBRobot

--- src/main/java/h03/Main.java
+++ src/main/java/h03/Main.java
@@ -1,10 +1,11 @@
 package h03;
 
 import fopbot.Direction;
+import fopbot.Robot;
 import fopbot.RobotFamily;
 import fopbot.World;
 import h03.robots.MultiFamilyRobot;
-import h03.robots.RGBRobots;
+import h03.robots.RGBRobot;
 import h03.robots.ChessBoardRobot;
 import h03.RobotSynchroniser;
 
@@ -21,7 +22,17 @@ public class Main {
         World.setSize(5, 5);
 
         final var robots = new Robot[]{
-            new Robot(0, 0,)
+            new MultiFamilyRobot(
+                0,
+                0,
+                new RobotFamily[]{RobotFamily.SQUARE_AQUA, RobotFamily.SQUARE_BLACK, RobotFamily.SQUARE_GREEN}
+            ),
+            new RGBRobot(1, 1, false),
+            new RGBRobot(1, 2, true),
+            new ChessBoardRobot(2, 0),
+            new ChessBoardRobot(2, 1),
+            new ChessBoardRobot(3, 0),
+            new ChessBoardRobot(3, 1),
         };
 
         World.setVisible(true);
--- src/main/java/h03/robots/RGBRobot.java
+++ src/main/java/h03/robots/RGBRobot.java
@@ -2,16 +2,14 @@ package h03.robots;
 
 import fopbot.RobotFamily;
 
-import static fopbot.RobotFamily.SQUARE_BLUE;
-import static fopbot.RobotFamily.SQUARE_RED;
-import static fopbot.RobotFamily.SQUARE_GREEN;
 
-public class RGBRobots extends MultiFamilyRobot{
+
+public class RGBRobot extends MultiFamilyRobot{
     private static final RobotFamily[] RGB = new RobotFamily[][SQUARE_RED, SQUARE_GREEN, SQUARE_BLUE];
 
     private static final RobotFamily[] BRG = new RobotFamily[][SQUARE_BLUE, SQUARE_RED, SQUARE_GREEN];
 
-    public RGBRobots(final int x, final int y, final boolean inverted){
+    public RGBRobot(final int x, final int y, final boolean inverted){
         super(x, y, !inverted ? BRG : RGB);
     }
 
