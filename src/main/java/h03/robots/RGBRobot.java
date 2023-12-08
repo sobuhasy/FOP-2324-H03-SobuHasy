b6d9ad1e980936411f69a1d800cd629bc6f53840
SobuHasy <sobu.hasy@hotmail.com>
2023-12-08 13:13:28 +0100

    Finally!!!! It works!!!!!

--- src/main/java/h03/robots/RGBRobot.java
+++ src/main/java/h03/robots/RGBRobot.java
@@ -3,10 +3,12 @@ package h03.robots;
 import fopbot.RobotFamily;
 
 
-public class RGBRobot extends MultiFamilyRobot{
-    private static final RobotFamily[] RGB = new RobotFamily[][RobotFamily.SQUARE_RED, RobotFamily.SQUARE_GREEN, RobotFamily.SQUARE_BLUE];
 
-    private static final RobotFamily[] BRG = new RobotFamily[][RobotFamily.SQUARE_BLUE, RobotFamily.SQUARE_RED, RobotFamily.SQUARE_GREEN];
+
+public class RGBRobot extends MultiFamilyRobot{
+    private static final RobotFamily[] RGB = new RobotFamily[]{RobotFamily.SQUARE_RED, RobotFamily.SQUARE_GREEN, RobotFamily.SQUARE_BLUE};
+
+    private static final RobotFamily[] BRG = new RobotFamily[]{RobotFamily.SQUARE_BLUE, RobotFamily.SQUARE_RED, RobotFamily.SQUARE_GREEN};
 
     public RGBRobot(final int x, final int y, final boolean inverted){
         super(x, y, !inverted ? BRG : RGB);
